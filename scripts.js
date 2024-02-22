8a60181771c69ff5897b44b9ccc1649d73817252
verma-ashish-k <ashish@ashishverma.ca>
2024-02-22 04:38:01 -0700

    First Commit

---
+++ scripts.js
@@ -0,0 +1,25 @@
+let switchBtn = document.querySelector('.btn');
+let body = document.body;
+let audioOn = document.getElementById('audioOn');
+let audioOff = document.getElementById('audioOff');
+
+function toggleBulb() {
+  if (body.classList.contains('on')) {
+    body.classList.remove('on'); // Remove the 'on' class for the light off
+    audioOff.play();
+  } else {
+    body.classList.add('on'); // Add the 'on' class for the light on
+    audioOn.play();
+  }
+}
+
+// Change the event listener to 'mousedown' instead of 'click'
+switchBtn.addEventListener('mousedown', toggleBulb);
+
+// Add event listener for 'keydown' on the document
+document.addEventListener('keydown', function (event) {
+  // Check if the pressed key is the spacebar (key code 32)
+  if (event.keyCode === 32) {
+    toggleBulb();
+  }
+});
