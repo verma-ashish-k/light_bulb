4e39367dc0dd12dd4127cd6fa87bc53b434c5526
verma-ashish-k <ashish@ashishverma.ca>
2024-02-22 05:02:48 -0700

    added audio

--- scripts.js
+++ scripts.js
@@ -2,14 +2,18 @@ let switchBtn = document.querySelector('.btn');
 let body = document.body;
 let audioOn = document.getElementById('audioOn');
 let audioOff = document.getElementById('audioOff');
+let creatureSounds = document.getElementById('creatureSounds');
 
 function toggleBulb() {
   if (body.classList.contains('on')) {
     body.classList.remove('on'); // Remove the 'on' class for the light off
     audioOff.play();
+    creatureSounds.play();
   } else {
     body.classList.add('on'); // Add the 'on' class for the light on
     audioOn.play();
+    creatureSounds.pause();
+    creatureSounds.currentTime = 0;
   }
 }
 
